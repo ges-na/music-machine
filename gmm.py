e3e532d42dbbf3b24a8abc03160c8fa2e2878fa3
gesina <gesinaface@gmail.com>
2018-03-25 15:16:24 -0400

    finished changing slashes

--- gmm.py
+++ gmm.py
@@ -56,8 +56,8 @@ class MusicMachine(object):
         copies files not present in output from input"""
         for root, dirs, files in os.walk(self.input_dir):
             ##input_dir_appended = 
-            trunc_root = root.replace(self.input_dir + "\\", "")
-            split_trunc_root = trunc_root.split("\\")
+            trunc_root = os.path.relpath(root, self.input_dir)
+            split_trunc_root = trunc_root.split(os.sep)
             array_length = len(split_trunc_root)
             if array_length == 2:
                 self.copy_album(files, trunc_root)
@@ -65,7 +65,7 @@ class MusicMachine(object):
     def copy_album(self, songs, path):
         for filename in songs:
             relative_path = os.path.join(path, filename)
-            norm_path = self.normalize_pathnames(relative_path)
+            norm_path = self.custom_normalize(relative_path)
             file_extension = filename.split('.').pop().lower()
             category = FILE_CATEGORY_MAP.get(file_extension)
             if category:
@@ -75,11 +75,11 @@ class MusicMachine(object):
                 copy2(file_from, file_to)
             #todo: check whether file exists already
 
-    def normalize_pathnames(self, pathname):
+    def custom_normalize(self, pathname):
         return pathname.replace(" ", "_")
 
     def create_output_dirs(self, file_to):
-        artist_album_folders = file_to.replace(self.output_dir, '').split('\\')
+        artist_album_folders = file_to.replace(self.output_dir, '').split(os.sep)
         artist_album_folders.pop()
         root = self.output_dir
         for folder in artist_album_folders:
