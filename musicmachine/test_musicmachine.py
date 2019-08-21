9ee06384c8a0e37001c01ad0370378bd61979fa4
Alligatortower <mrweed@gmail.com>
2019-08-21 17:34:29 -0400

    fixing some structural stuff

--- musicmachine/test_musicmachine.py
+++ musicmachine/test_musicmachine.py
@@ -1,41 +1,41 @@
 import os
 import unittest
- 
+
 from app import app, db
+from views import *
 
 TEST_DB = 'test.db'
- 
- 
+
+
 class BasicTests(unittest.TestCase):
- 
+
     ############################
     #### setup and teardown ####
     ############################
- 
+
     # executed prior to each test
     def setUp(self):
         app.config['TESTING'] = True
         app.config['WTF_CSRF_ENABLED'] = False
         app.config['DEBUG'] = False
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
-            os.path.join(app.config['BASEDIR'], TEST_DB)
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
         self.app = app.test_client()
         db.drop_all()
         db.create_all()
- 
+
     # executed after each test
     def tearDown(self):
         pass
- 
- 
+
+
 ###############
 #### tests ####
 ###############
- 
+
     def test_main_page(self):
         response = self.app.get('/', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
- 
- 
+
+
 if __name__ == "__main__":
     unittest.main()
